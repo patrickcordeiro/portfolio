b0fc5d5308c112be9127bcf95df26043fb3e4db9
patrickcordeiro <patrick.cordeiro30@gmail.com>
2023-02-01 09:43:37 -0300

    Adicionando dados pessoais de contato

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
+  nameUser: "Patrick Cordeiro",
+  githubUser: "patrickcordeiro",
+  linkedinUser: "patrickcordeiro",
+  whatsappNumber: "+5511996390335",
+  emailUser: "patrick.cordeiro30@gmail.com",
+  instagramUser: "patrickpalhano_dev",
   facebookUser: "",
 };
